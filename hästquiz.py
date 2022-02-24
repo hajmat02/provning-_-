dbd99a7d29a43292c476f329ad1972481ea7ee69
hajmat02 <maja.wallberg@elev.ga.ntig.se>
2022-02-24 20:24:05 +0100

    Update hästquiz.py
    la till en loop ochh den fukar :)

--- hästquiz.py
+++ hästquiz.py
@@ -1,7 +1,10 @@
-from random import randint
+
 
 
 start = input("Välkommen till Häst Quizet, tryck enter för att fortsätta")
+
+
+
 
 info = input('''Du kommer nu får frågor och med de frågorna kommer du ha tre alternativ,
  A B & C. I slutet kommer du att få veta hur många rätt du fick och hur mycket du kan om hästar,
@@ -9,61 +12,57 @@ info = input('''Du kommer nu får frågor och med de frågorna kommer du ha tre 
 
 svar = 0
 
-fråga1 = input('''Vad heter färgen på hästen om den är vit?
-a: Månstoft
-b: Skimmel
-c: Vit
- ''')
+slut = "0"
 
-if fråga1 == "b":
-    svar += 1
-else:
-    svar = svar
+while slut != "2":
+    fråga1 = input('''Vad heter färgen på hästen om den är vit?
+    a: Månstoft
+    b: Skimmel
+    c: Vit
+    ''')
 
-fråga2 = input('''Vad heter det man har under sadeln?
-a: Shabrak
-b: Sadelskydd
-c: Sadelmatta
- ''')
+    if fråga1 == "b":
+        svar += 1
 
-if fråga2 == "a":
-    svar += 1
-else:
-    svar = svar
+    fråga2 = input('''Vad heter det man har under sadeln?
+    a: Shabrak
+    b: Sadelskydd
+    c: Sadelmatta
+    ''')
 
-fråga3 = input('''Vad heter en tjej häst?
-a: Tik
-b: Sto
-c: Tacka
- ''')
+    if fråga2 == "a":
+        svar += 1
 
-if fråga3 == "b":
-    svar += 1
-else:
-    svar = svar
+    fråga3 = input('''Vad heter en tjej häst?
+    a: Tik
+    b: Sto
+    c: Tacka
+    ''')
 
-fråga4 = input('''Vad heter en kastrerad kill häst?
-a: Valack
-b: Hingst
-c: Hane
- ''')
+    if fråga3 == "b":
+        svar += 1
 
-if fråga4 == "a":
-    svar += 1
-else:
-    svar = svar
+    fråga4 = input('''Vad heter en kastrerad kill häst?
+    a: Valack
+    b: Hingst
+    c: Hane
+    ''')
 
-fråga5 = input('''Hur gammal blir vanligt vis en häst?
-a: 15-20
-b: 30-45
-c: 20-35
- ''')
+    if fråga4 == "a":
+        svar += 1
 
-if fråga5 == "c":
-    svar += 1
-else:
-    svar = svar
+    fråga5 = input('''Hur gammal blir vanligt vis en häst?
+    a: 15-20
+    b: 30-45
+    c: 20-35
+    ''')
+
+    if fråga5 == "c":
+     svar += 1
 
 
 
-print(f"Nu är quizet slut och du fick {svar} av 5")
+    print(f"Nu är quizet slut och du fick {svar} av 5")
+    slut = input('''
+    Vill du spela igen tyck [1]
+    Om inte tryck [2]''')
